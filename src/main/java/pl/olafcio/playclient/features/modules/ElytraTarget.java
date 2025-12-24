ba3d1daca4701b556a9df4ca72c57c9ca0d213bf
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2025-12-25 00:34:52 +0100

    [ElytraTarget] Make it packet-based, auto-firework

--- src/main/java/pl/olafcio/playclient/features/modules/ElytraTarget.java
+++ src/main/java/pl/olafcio/playclient/features/modules/ElytraTarget.java
@@ -12,6 +12,11 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import pl.olafcio.playclient.PlayAddon;
 
 public class ElytraTarget extends Module {
@@ -43,15 +48,75 @@ public class ElytraTarget extends Module {
 
     @EventHandler(priority = -1)
     public void onTick(TickEvent.Pre event) {
-        if (!mc.player.isOnGround() && mc.player.getInventory().getStack(EquipmentSlot.BODY.getIndex()).getItem() == Items.ELYTRA) {
-            mc.player.lookAt(EntityAnchorArgumentType.EntityAnchor.FEET, target.getEntityPos());
+        if (target != null) {
+            if (!target.isAlive()) {
+                target = null;
+                return;
+            }
+        } else return;
+
+        // 32==9*4==player inventory bars combined
+        var inventory = mc.player.getInventory();
+        if (
+                !mc.player.isOnGround() &&
+                inventory.getStack(32 + EquipmentSlot.BODY.getIndex()).getItem() == Items.ELYTRA
+        ) {
+            var rot = lookAt(EntityAnchorArgumentType.EntityAnchor.FEET, target.getEntityPos());
+
             mc.options.forwardKey.setPressed(true);
+            mc.options.jumpKey.setPressed(true);
+
+            if (mc.player.getVelocity().y < 0) {
+                var stacks = inventory.getMainStacks();
+                var index = 0;
+
+                for (var stack : stacks) {
+                    if (stack.getItem() == Items.FIREWORK_ROCKET) {
+                        mc.player.networkHandler.sendPacket(inventory.createSlotSetPacket(index));
+                        mc.player.networkHandler.sendPacket(inventory.createSlotSetPacket(inventory.getSelectedSlot()));
+                        mc.player.networkHandler.sendPacket(new PlayerInteractItemC2SPacket(
+                                Hand.MAIN_HAND,
+                                0,
+                                rot.yaw, rot.pitch
+                        ));
+
+                        break;
+                    }
+
+                    index++;
+                }
+            }
 
             just = true;
         } else if (just) {
             just = false;
+
             mc.options.forwardKey.setPressed(false);
+            mc.options.jumpKey.setPressed(false);
         }
+    }
+
+    protected record Rotation(float yaw, float pitch) {}
+
+    protected Rotation lookAt(EntityAnchorArgumentType.EntityAnchor anchorPoint, Vec3d target) {
+        var self = mc.player;
+        var vec3d = anchorPoint.positionAt(self);
+
+        double d = target.x - vec3d.x;
+        double e = target.y - vec3d.y;
+        double f = target.z - vec3d.z;
+        double g = Math.sqrt(d * d + f * f);
+
+        var pitch = MathHelper.wrapDegrees((float)(-(MathHelper.atan2(e, g) * (double)(180F / (float)Math.PI))));
+        var yaw = MathHelper.wrapDegrees((float)(MathHelper.atan2(f, d) * (double)(180F / (float)Math.PI)) - 90.0F);
+
+        self.networkHandler.sendPacket(new PlayerMoveC2SPacket.Full(
+                self.getEntityPos(),
+                yaw, pitch,
+                self.isOnGround(), self.horizontalCollision
+        ));
+
+        return new Rotation(yaw, pitch);
     }
 
     @EventHandler
