bca1e23e53b213e55578f0806face188dd16f22b
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2025-12-24 20:54:20 +0100

    [ElytraTarget] Empty module

---
+++ src/main/java/pl/olafcio/playclient/features/modules/ElytraTarget.java
@@ -0,0 +1,10 @@
+package pl.olafcio.playclient.features.modules;
+
+import meteordevelopment.meteorclient.systems.modules.Module;
+import pl.olafcio.playclient.PlayAddon;
+
+public class ElytraTarget extends Module {
+    public ElytraTarget() {
+        super(PlayAddon.CATEGORY, "ElytraTarget", "Flies onto a player with elytra.");
+    }
+}
