b7bd54cc100a5c8935e7ffcdf297ce753e439be1
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2025-12-27 16:55:37 +0100

    [InventoryChat] Improve chatbox event handling

--- src/main/java/pl/olafcio/playclient/mixin/InventoryScreenMixin.java
+++ src/main/java/pl/olafcio/playclient/mixin/InventoryScreenMixin.java
@@ -1,30 +1,56 @@
 package pl.olafcio.playclient.mixin;
 
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.Click;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.screen.ingame.RecipeBookScreen;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import net.minecraft.client.input.KeyInput;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import pl.olafcio.playclient.util.screen.ChatPromptElement;
 
 @Mixin(InventoryScreen.class)
-public class InventoryScreenMixin extends Screen {
-    protected InventoryScreenMixin(Text title) {
-        super(title);
+public abstract class InventoryScreenMixin extends RecipeBookScreen<PlayerScreenHandler> {
+    private InventoryScreenMixin(PlayerScreenHandler handler, RecipeBookWidget<?> recipeBook, PlayerInventory inventory, Text title) {
+        super(handler, recipeBook, inventory, title);
     }
+
+    @Unique
+    private ChatPromptElement cpe;
 
     @Inject(at = @At("CTOR_HEAD"), method = "<init>")
     public void construct(PlayerEntity player, CallbackInfo ci) {
         int elHeight = client.textRenderer.fontHeight + 8;
-        addDrawableChild(new ChatPromptElement(
+        addDrawableChild(cpe = new ChatPromptElement(
                 client.textRenderer,
                 0, height - elHeight,
                 100,
                 elHeight,
                 Text.of("chat box")
         ));
+    }
+
+    @Override
+    public boolean mouseClicked(Click click, boolean doubled) {
+        if (!cpe.isHovered())
+            cpe.setFocused(false);
+
+        return super.mouseClicked(click, doubled);
+    }
+
+    @Override
+    public boolean keyPressed(KeyInput input) {
+        return (
+                input.key() != GLFW.GLFW_KEY_ESCAPE &&
+                cpe.isFocused()
+        ) ? cpe.keyPressed(input) : super.keyPressed(input);
     }
 }
