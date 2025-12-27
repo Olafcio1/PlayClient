719e8b410422bf8a963ee8780637c585ac4e0d97
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2025-12-27 20:42:33 +0100

    [Notifications] More customization

--- src/main/java/pl/olafcio/playclient/features/hud/notifications/Notifications.java
+++ src/main/java/pl/olafcio/playclient/features/hud/notifications/Notifications.java
@@ -2,6 +2,9 @@ package pl.olafcio.playclient.features.hud.notifications;
 
 import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.settings.ColorSetting;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
@@ -45,30 +48,70 @@ public class Notifications extends HudElement {
         NotificationStore.EVENT_BUS.unsubscribe(this);
     }
 
-    protected final ColorSetting background = settings.getDefaultGroup().add(new ColorSetting.Builder()
+    protected final SettingGroup sgSize = settings.createGroup("Size");
+    protected final IntSetting width = sgSize.add(new IntSetting.Builder()
+            .name("width")
+            .description("The notification width.")
+            .defaultValue(200)
+            .min(1)
+            .noSlider()
+    .build());
+    protected final IntSetting height = sgSize.add(new IntSetting.Builder()
+            .name("height")
+            .description("The notification height.")
+            .defaultValue(84)
+            .min(1)
+            .noSlider()
+    .build());
+
+    protected final SettingGroup sgColors = settings.createGroup("Colors");
+    protected final ColorSetting background = sgColors.add(new ColorSetting.Builder()
             .name("background")
             .description("The background of a notification.")
             .defaultValue(new Color(19, 59, 98))
     .build());
-
-    protected final ColorSetting lineBackground = settings.getDefaultGroup().add(new ColorSetting.Builder()
-            .name("background")
-            .description("The background of a notification.")
+    protected final ColorSetting lineBackground = sgColors.add(new ColorSetting.Builder()
+            .name("line-background")
+            .description("The background of the time line.")
             .defaultValue(new Color(39, 79, 118))
     .build());
-
-    protected final ColorSetting textColor = settings.getDefaultGroup().add(new ColorSetting.Builder()
+    protected final ColorSetting textColor = sgColors.add(new ColorSetting.Builder()
             .name("text-color")
             .description("The text color of a notification.")
             .defaultValue(new Color(235, 235, 235))
+    .build());
+
+    protected final SettingGroup sgBorder = settings.createGroup("Border");
+    protected final IntSetting borderSize = sgBorder.add(new IntSetting.Builder()
+            .name("border-size")
+            .description("The border size.")
+            .defaultValue(1)
+            .sliderRange(1, 10)
+    .build());
+    protected final ColorSetting borderColor = sgBorder.add(new ColorSetting.Builder()
+            .name("border-color")
+            .description("The border color.")
+            .defaultValue(new Color(59, 99, 138))
+    .build());
+
+    protected final SettingGroup sgNumbers = settings.createGroup("Numbers");
+    protected final DoubleSetting lineHeight = sgNumbers.add(new DoubleSetting.Builder()
+            .name("line-height")
+            .description("The height of the time line.")
+            .defaultValue(2d)
+    .build());
+    protected final IntSetting gap = sgNumbers.add(new IntSetting.Builder()
+            .name("gap")
+            .description("The gap between each notification.")
+            .defaultValue(8)
     .build());
 
     protected int id = 0;
 
     @Override
     public void render(HudRenderer renderer) {
-        var width = 200;
-        var height = 84;
+        var width = this.width.get();
+        var height = this.height.get();
 
         setSize(width, height);
 
@@ -76,19 +119,18 @@ public class Notifications extends HudElement {
             var now = System.currentTimeMillis();
 
             var offset = y;
-            var gap = 8;
+            var gap = this.gap.get();
 
             for (var entry : notifications.entrySet()) {
                 var notif = entry.getKey();
                 var timeEnd = entry.getValue();
 
                 var thisY = offset;
-                float anim;
 
                 if (timeEnd - DURATION >= now) {
                     // showing
                     long time = (timeEnd - DURATION) - now;
-                    anim = (float)time / (float)SHOW_DURATION;
+                    float anim = (float)time / (float)SHOW_DURATION;
 
                     thisY += (int) ((float)(MeteorClient.mc.getWindow().getHeight() - thisY) * anim);
                 } else if (timeEnd >= now) {
@@ -96,19 +138,17 @@ public class Notifications extends HudElement {
                 } else if (timeEnd + HIDE_DURATION >= now) {
                     // hiding
                     long time = (timeEnd + HIDE_DURATION) - now;
-                    anim = (float)time / (float)HIDE_DURATION;
+                    float anim = (float)time / (float)HIDE_DURATION;
 
                     thisY += (int) ((float)(MeteorClient.mc.getWindow().getHeight() - thisY) * (1 - anim));
+                    offset += (int) ((float)(height + gap) * (1 - anim));
                 } else {
                     toRemove.add(notif);
                     continue;
                 }
 
-                renderer.quad(x, thisY, width, height, background.get());
-                renderer.line(x, thisY + height - 1, x, thisY + height, lineBackground.get());
-
-                renderer.text("Meteor • " + notif.prefixTitle, x + 3, thisY + 3, textColor.get(), true, .8d);
-                drawMultiText(renderer, notif.msg, x + 3, thisY + 3 + renderer.textHeight(true, .7d) + 3, textColor.get(), true, 1d);
+                var anim = (float)(timeEnd - now) / (float)DURATION;
+                drawNotification(renderer, thisY, width, height, anim, notif);
 
                 offset -= height + gap;
             }
@@ -117,6 +157,21 @@ public class Notifications extends HudElement {
             if (notifications.isEmpty())
                 id = 0;
         }
+    }
+
+    protected void drawNotification(HudRenderer renderer, int y, int width, int height, float anim, NotificationEvent notif) {
+        renderer.drawContext.enableScissor(x, y, x + width, this.y + height);
+
+        renderer.quad(x, y, width, height, background.get());
+        renderer.quad(x, y + height - lineHeight.get(), width * Math.min(1, 1 - anim), lineHeight.get(), lineBackground.get());
+
+        renderer.text("Meteor • " + notif.prefixTitle, x + 3, y + 3, textColor.get(), true, .8d);
+        drawMultiText(renderer, notif.msg, x + 3, y + 3 + renderer.textHeight(true, .7d) + 3, textColor.get(), true, 1d);
+
+        for (int r = 0; r < borderSize.get(); r++)
+            renderer.drawContext.drawStrokedRectangle(x + r, y + r, width - r, height - r, borderColor.get().getPacked());
+
+        renderer.drawContext.disableScissor();
     }
 
     protected void drawMultiText(
